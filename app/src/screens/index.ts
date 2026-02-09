898ac5dd18da2841efffeede7631766a60590288
Noah Cherel <91745522+NoahCherel@users.noreply.github.com>
2026-02-10 00:16:24 +0100

    [+] Add OnboardingScreen processus

--- app/src/screens/index.ts
+++ app/src/screens/index.ts
@@ -1,3 +1,6 @@
 export { default as AuthScreen } from './AuthScreen';
 export { default as HomeScreen } from './HomeScreen';
 export { default as SplashScreen } from './SplashScreen';
+export { default as OnboardingScreen } from './OnboardingScreen';
+export { default as EditProfileScreen } from './EditProfileScreen';
+export { default as MyProfileScreen } from './MyProfileScreen';
