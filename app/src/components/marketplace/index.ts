40d99b19b43984c6831eca594da7a48f9fca32e9
Noah Cherel <91745522+NoahCherel@users.noreply.github.com>
2026-02-10 23:19:45 +0100

    [+] feat: Add MentorDetailScreen component for displaying mentor profiles

---
+++ app/src/components/marketplace/index.ts
@@ -0,0 +1,5 @@
+export { default as MentorCard } from './MentorCard';
+export { default as SearchBar } from './SearchBar';
+export { default as FilterBar } from './FilterBar';
+export type { ActiveFilters } from './FilterBar';
+export { default as FilterModal } from './FilterModal';
